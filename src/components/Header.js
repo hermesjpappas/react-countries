63e00aedfb6e165efb7b11d9f916036dca00c6bb
Hermes J. Pappas <hjp@hjp.gr>
2022-02-07 13:03:34 +0200

    Make and style Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons'
+
 
 export default function Header() {
-  return <div></div>;
+  return <div className="flex items-center w-screen bg-white text-gray-700 p-4 shadow-md">
+  {/* change above to flex justify-between when/if you put a dark mode button */}
+    <div className="flex items-center justify-start gap-3 text-lg">
+      <FontAwesomeIcon icon={faGlobe}/>
+      <p>Around The World</p>
+    </div>
+  </div>;
 }
