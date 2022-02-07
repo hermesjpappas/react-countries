ca77843af3bb9152b0f6cc885edc0f428c0b7436
Hermes J. Pappas <hjp@hjp.gr>
2022-02-07 15:49:48 +0200

    Small changes to header and font

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import { faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 
 export default function Header() {
-  return <div className="flex items-center w-screen bg-white text-gray-700 p-4 shadow-md">
+  return <div className="flex items-center w-screen bg-white text-gray-700 p-3 shadow-md">
   {/* change above to flex justify-between when/if you put a dark mode button */}
-    <div className="flex items-center justify-start gap-3 text-lg">
-      <FontAwesomeIcon icon={faGlobe}/>
-      <p>Around The World</p>
+    <div className="flex items-center justify-start gap-3">
+      <FontAwesomeIcon icon={faGlobe} className="text-xl"/>
+      <p className="pt-1">Around The World</p>
     </div>
   </div>;
 }
