04e1dca4e44fe514b7a247a7b54d9668bed0e4f2
Hermes J. Pappas <hjp@hjp.gr>
2022-02-10 10:40:10 +0200

    Fix Footer text to fit mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react";
 function Footer() {
   return (
     <div className='w-full mt-12 p-4 flex flex-col items-center bg-gray-700 text-gray-400 absolute bottom-0'>
-      <p>Design and code by <a  className=" text-white" href="https://github.com/hermesjpappas/" target="_blank">Hermes J. Pappas</a></p>
-      <p className='text-sm'>
+      <p className="text-sm sm:text-base">Design and code by <a  className=" text-white" href="https://github.com/hermesjpappas/" target="_blank">Hermes J. Pappas</a></p>
+      <p className='text-xs sm:text-sm'>
         Data provided by the <a className=" text-white" href='https://restcountries.com/' target="_blank">REST Countries API</a>
       </p>
     </div>
