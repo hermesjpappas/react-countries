64c2b12865be5f3478801502d0f79f3a00048340
Hermes J. Pappas <hjp@hjp.gr>
2022-02-10 10:33:46 +0200

    Update footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from "react";
 function Footer() {
   return (
     <div className='w-full mt-12 p-4 flex flex-col items-center bg-gray-700 text-gray-400 absolute bottom-0'>
-      <p>Design and code by <a  className=" text-white" href="">Hermes J. Pappas</a></p>
+      <p>Design and code by <a  className=" text-white" href="https://github.com/hermesjpappas/" target="_blank">Hermes J. Pappas</a></p>
       <p className='text-sm'>
-        Data provided by the <a className=" text-white" href='https://restcountries.com/'>REST Countries API</a>
+        Data provided by the <a className=" text-white" href='https://restcountries.com/' target="_blank">REST Countries API</a>
       </p>
     </div>
   );
