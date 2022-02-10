0dcb7e8339e00b62c04b6139bd45b6e88e0532d8
Hermes J. Pappas <hjp@hjp.gr>
2022-02-10 09:34:18 +0200

    Add alt description to Country flags

--- src/components/Country.js
+++ src/components/Country.js
@@ -7,7 +7,7 @@ export default function Country({country}) {
     <Link to={"/" + country.cca3}>
       <span className="flex h-full">
         <div className='flex flex-col w-60 sm:w-72 md:w-80 bg-gray-200 rounded-md shadow-lg'>
-          <img className='w-full h-1/2 rounded-t-md' src={country.flags.png} />
+          <img className='w-full h-1/2 rounded-t-md' src={country.flags.png} alt={country.name.common + " flag"}/>
             <div className="h-1/2 p-4 text-sm flex flex-col justify-center text-gray-700">
               <p className="text-xl font-bold pb-4">{country.name.common}</p>
               <p><span className="font-bold">Population:</span> {country.population.toLocaleString()}</p>
