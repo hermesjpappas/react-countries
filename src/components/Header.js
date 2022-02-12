9752fae7d78f0d7707510cc3d0bdd3faa5281967
Hermes J. Pappas <hjp@hjp.gr>
2022-02-12 11:28:09 +0200

    Implement dark mode toggle with icons

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,10 +2,15 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGlobe, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
+import  useDarkMode  from '../useDarkMode';
 
 export default function Header() {
+
+  const [darkMode, toggleDarkMode] = useDarkMode();
+
+
   return (
-    <div className='flex w-full items-center bg-white text-gray-700 dark:bg-gray-600 dark:text-gray-300 p-3 shadow-lg lg:p-6'>
+    <div className='flex w-full justify-between items-center bg-white text-gray-700 dark:bg-gray-600 dark:text-gray-300 p-3 shadow-lg lg:p-6'>
       {/* change above to flex justify-between when/if you put a dark mode button */}
       <Link to='/'>
         <div className='flex items-center justify-start gap-3'>
@@ -18,6 +23,13 @@ export default function Header() {
           </p>
         </div>
       </Link>
+
+      <div className="pt-1 pr-1">
+          <FontAwesomeIcon icon={darkMode ? faSun : faMoon} 
+            className="text-lg md:text-xl lg:text-2xl cursor-pointer"
+            onClick={toggleDarkMode}
+          />
+        </div>
     </div>
   );
 }
