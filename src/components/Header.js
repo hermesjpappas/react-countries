fda5fcccb6457291083ea71b753d987196957b58
Hermes J. Pappas <hjp@hjp.gr>
2022-02-12 11:21:06 +0200

    Make custom hook for toggling dark mode

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGlobe } from "@fortawesome/free-solid-svg-icons";
+import { faGlobe, faMoon, faSun } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
   return (
---
+++ src/useDarkMode.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import { useEffect } from 'react/cjs/react.development';
+
+export default function useDarkMode() {
+ 
+  const [darkMode, setDarkMode] = useState(
+    () => localStorage.theme === "dark"
+  );
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+
+    //get the html element
+    const html = window.document.documentElement;
+
+    //find out what the previous theme was, remove it
+    const prevTheme = darkMode ? "light" : "dark";
+    html.classList.remove(prevTheme);
+
+    //add the opposite theme
+    const nextTheme = darkMode ? "dark" : "light";
+    html.classList.add(nextTheme);
+
+    //add it to local storage
+    localStorage.setItem("theme", nextTheme);
+
+  }, [darkMode]);
+
+  return [darkMode, toggleDarkMode];
+
+}
