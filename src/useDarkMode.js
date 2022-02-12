a713154bad6e11eb35cd0e1fb9cdcda915b10f7e
Hermes J. Pappas <hjp@hjp.gr>
2022-02-12 11:45:10 +0200

    Fix import

--- src/useDarkMode.js
+++ src/useDarkMode.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react'
-import { useEffect } from 'react/cjs/react.development';
+import React, { useState, useEffect } from 'react'
 
 export default function useDarkMode() {
  
