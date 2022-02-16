f9b927f2563b36f106c394a5f0c0f920b302668b
Hermes J. Pappas <hjp@hjp.gr>
2022-02-16 15:07:15 +0200

    Solve popup button issue by removing inner toggle on button

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -5,7 +5,7 @@ function Popup({link, trigger, toggle}) {
     <div className="z-10 fixed top-0 left-0 w-full h-full bg-gray-500/90 flex flex-col items-center overflow-auto"
     onClick={toggle}>
       <div className="relative p-8 w-full max-w-6xl flex flex-col items-center gap-6 max-h-full">
-          <button onClick={toggle} className='bg-gray-700 text-gray-300 px-6 py-2 rounded shadow-md shadow-gray-700 self-start'>Close</button>
+          <button className='bg-gray-700 text-gray-300 px-6 py-2 rounded shadow-md shadow-gray-700 self-start'>Close</button>
           <img src={link} />
       </div>
     </div>
